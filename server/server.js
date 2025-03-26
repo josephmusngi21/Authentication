5578df0a94072e399a62a368d22e5777a5dd0c9e
Josephmusngi21 <josephmusngi7@gmail.com>
2025-03-25 23:50:56 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World\n');
+}).listen(port);
