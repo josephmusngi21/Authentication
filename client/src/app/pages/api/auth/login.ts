95994e3d89ce7e17bbc712a9b4b72de26bb204ae
BennDoverr <92238696+BennDoverr@users.noreply.github.com>
2025-03-26 23:41:53 -0700

    commented broken code

--- client/src/app/pages/api/auth/login.ts
+++ client/src/app/pages/api/auth/login.ts
@@ -7,14 +7,14 @@ export default async function handler(
 ) {
   try {
     const { email, password } = req.body
-    await signIn('credentials', { email, password })
+    // await signIn('credentials', { email, password })
  
     res.status(200).json({ success: true })
   } catch (error) {
-    if (error.type === 'CredentialsSignin') {
-      res.status(401).json({ error: 'Invalid credentials.' })
-    } else {
-      res.status(500).json({ error: 'Something went wrong.' })
-    }
+    // if (error.type === 'CredentialsSignin') {
+    //   res.status(401).json({ error: 'Invalid credentials.' })
+    // } else {
+    //   res.status(500).json({ error: 'Something went wrong.' })
+    // }
   }
 }
