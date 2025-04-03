82a1e85b936967461225847c83a077baab9f7393
josephmusngi21 <josephmusngi7@gmail.com>
2025-04-03 12:45:46 -0700

    connected to mongodatabase

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
-var http = require('http');
-const port = process.env.PORT || 3000;
+import express from "express"; 
+const mongoose = require("mongoose"); 
+const authRoutes = require("./routes/auth"); 
+const cors = require("cors");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World\n');
-}).listen(port);
+const app = express();
+const PORT = 3001;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+
+  app.use(cors());
+  app.use(express.json());
+  app.use("/api/auth", authRoutes); 
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
