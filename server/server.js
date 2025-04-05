6edd9eb8c62f193823eea8eb2e79c6975b410ff8
josephmusngi21 <josephmusngi7@gmail.com>
2025-04-04 17:39:23 -0700

    added comments to connection

--- server/server.js
+++ server/server.js
@@ -9,18 +9,20 @@ const app = express();
 const PORT = 3001;
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI, { //this connects to mongo database
+    useNewUrlParser: true,    // this lets us use the new URL parser for MongoDB
+    useUnifiedTopology: true, // this lets us use the new topology engine for MongoDB
+  })    // then and catch to check connection status on database
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 
-  app.use(cors());
-  app.use(express.json());
-  app.use("/api/auth", authRoutes); 
+  app.use(cors()); // this allows us to connect to the database and create a server
+  app.use(express.json()); // this allows us to parse JSON data from the request body
+  app.use("/api/auth", authRoutes);  // this uses the auth routes that we made in /routes/auth.js
 
+
+  // this allows the user to know if the connection was successful or not
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
