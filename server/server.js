111526c9b406e130152733f4ccc025874bdf9d2b
josephmusngi21 <josephmusngi7@gmail.com>
2025-04-04 18:28:25 -0700

    imported port from .env

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.js";
 import cors from "cors";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose
